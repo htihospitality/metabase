0bc0930f523110dc143594c8cf6142e0d95040aa
Kevin O'Donnell <hti-kevin@bitbucket.org>
2020-07-16 11:48:20 +0200

    changed logo.

--- resources/locales.clj
+++ resources/locales.clj
@@ -1,5 +1,5 @@
 {
-  :locales  #{"en" "zh" "zh-HK" "zh-TW" "uk" "fr" "nl" "tr" "sk" "it" "es" "nb" "ca" "de" "fa" "ja" "vi" "pl" "pt" "ru" }
+  :locales  #{"en" "ca" "de" "es" "fa" "fr" "it" "ja" "nl" "pl" "pt" "ru" "sk" "tr" "zh-HK" "zh-TW" "zh" }
   :packages ["metabase"]
   :bundle   "metabase.Messages"
 }
